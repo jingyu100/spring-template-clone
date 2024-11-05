d20cc75ee69f398b408235b825e25c2c708d0b6b
jingyu100 <jingyu100@naver.com>
2024-11-05 14:58:36 +0900

    Add navbar without dropdown list

---
+++ spring-template/src/main/java/rpa0108/spring_template/Controller/MainController.java
@@ -0,0 +1,13 @@
+package rpa0108.spring_template.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String mainPage() {
+        return "main";
+    }
+}
