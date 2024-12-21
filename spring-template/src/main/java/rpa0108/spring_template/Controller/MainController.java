87fc7768389f3b4172bb3300967c47f2c8f6f5a0
jingyu100 <jingyu100@naver.com>
2024-12-21 15:57:48 +0900

    회원가입 페이지 수정

--- spring-template/src/main/java/rpa0108/spring_template/Controller/MainController.java
+++ spring-template/src/main/java/rpa0108/spring_template/Controller/MainController.java
@@ -3,11 +3,7 @@ package rpa0108.spring_template.Controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import rpa0108.spring_template.entity.User;
 import rpa0108.spring_template.service.UserService;
-
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
